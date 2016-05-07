14c8bbe165e76052ea2c91076d5ecdb8f7ece588
Christopher Reitz <christopher@reitz.re>
2016-05-07 19:57:58 +0200

    Add ssl support

--- main.go
+++ main.go
@@ -6,23 +6,70 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/user"
 )
 
 func main() {
 	startServer()
 }
 
-func startServer() {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "3000"
-		fmt.Printf("[mservice] listening on port %s", port)
+func isRoot() bool {
+	usr, _ := user.Current()
+	return usr.Uid == "0"
+}
+
+func certsExists(certFile string, keyFile string) bool {
+	for _, file := range []string{certFile, keyFile} {
+		_, err := os.Stat(file)
+		if os.IsNotExist(err) {
+			return false
+		}
 	}
 
+	return true
+}
+
+func findPort(ssl bool) string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		user := !isRoot()
+		if ssl {
+			if user {
+				port = "4443"
+			} else {
+				port = "443"
+			}
+
+		} else {
+			if user {
+				port = "8080"
+			} else {
+				port = "80"
+			}
+		}
+	}
+
+	return port
+}
+
+func startServer() {
+	certFile := "certs/server.pem"
+	keyFile := "certs/server.key"
+	ssl := certsExists(certFile, keyFile)
+	port := findPort(ssl)
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":"+port, router))
+
+	log.Printf("start listening on port %s", port)
+
+	var err error
+	if ssl {
+		err = http.ListenAndServeTLS(":"+port, certFile, keyFile, router)
+	} else {
+		err = http.ListenAndServe(":"+port, router)
+	}
+
+	log.Fatal(err)
 }
