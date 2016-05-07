44814c2bb8b6467b8b0f4023abc4464f09cafc0e
Christopher Reitz <christopher@reitz.re>
2016-05-07 16:40:37 +0200

    Render README in index route

--- index_controller.go
+++ index_controller.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
+
+	"github.com/russross/blackfriday"
 )
 
+var readMe string
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome!\n")
+	if len(readMe) == 0 {
+		initReadMe()
+	}
+
+	fmt.Fprint(w, readMe)
+}
+
+func initReadMe() {
+	rawHtml, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	rawMarkdown, err2 := ioutil.ReadFile("README.md")
+	if err2 != nil {
+		panic(err2)
+	}
+
+	parsedMarkdown := blackfriday.MarkdownBasic(rawMarkdown)
+	html := string(rawHtml[:])
+	markdown := string(parsedMarkdown[:])
+	readMe = strings.Replace(html, "<!-- README.md -->", markdown, 1)
 }
